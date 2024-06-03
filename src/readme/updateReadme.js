a23c5310ed7545b4d41ac0be996e9d0fbcc16411
Felipe Arce <arceprogramando@gmail.com>
2024-06-02 23:56:25 -0300

    test

--- src/readme/updateReadme.js
+++ src/readme/updateReadme.js
@@ -2,10 +2,8 @@ import fs from 'fs/promises';
 
 export const updateReadme = async (data) => {
   try {
-
     const { mainVideo, lastVideos } = data;
 
-    // Estructura para el nuevo contenido de la sección de YouTube
     let youtubeContent = `<div class="Youtube-Content">
 # Canal de YouTube
 
@@ -19,25 +17,22 @@ export const updateReadme = async (data) => {
     lastVideos.forEach((video, index) => {
       youtubeContent += `
 <a href='${video.url}' target='_blank'>
-  <img width='30%' src='${video.thumbnail}' alt='Últimos videos ${index}' />
+  <img width='30%' src='${video.thumbnail}' alt='Últimos videos ${index + 1}' />
 </a>`;
     });
 
-    youtubeContent += `</div>`;
+    youtubeContent += `
+</div>`;
 
-    // Leer el contenido existente del README.md
     let readmeContent = await fs.readFile('README.md', 'utf8');
 
-    // Actualizar solo la sección de YouTube
     const youtubeSectionRegex = /<div class="Youtube-Content">[\s\S]*?<\/div>/;
     if (youtubeSectionRegex.test(readmeContent)) {
       readmeContent = readmeContent.replace(youtubeSectionRegex, youtubeContent);
     } else {
-      // Si no existe, agregar al final del contenido
       readmeContent += `\n${youtubeContent}`;
     }
 
-    // Escribir el contenido actualizado en el archivo README.md
     await fs.writeFile('README.md', readmeContent, 'utf8');
     console.log('README.md actualizado exitosamente');
   } catch (error) {
